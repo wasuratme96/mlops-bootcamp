e411e9e75d7fa83137f00fb1eeb5bfa00954deb9
Wasurat S <wasurat_me96@outlook.com>
2022-08-14 23:26:28 +0700

    prefect local sever testing

--- 03-orchestration/prefect_flow.py
+++ 03-orchestration/prefect_flow.py
@@ -1,10 +1,7 @@
-from audioop import add
 import pandas as pd
 import pickle
-import os
 
 from sklearn.feature_extraction import DictVectorizer
-from sklearn.linear_model import LinearRegression, Lasso, Ridge
 from sklearn.metrics import mean_squared_error
 
 import xgboost as xgb
@@ -14,9 +11,10 @@ from hyperopt.pyll import scope
 
 import mlflow
 
-mlflow.set_tracking_uri("sqlite:///mlflow.db")
-mlflow.set_experiment("nyc-taxi-experiment")
+from prefect import flow, task
+from prefect.task_runners import SequentialTaskRunner
 
+@task
 def read_dataframe(filename):
     df = pd.read_parquet(filename)
 
@@ -33,11 +31,8 @@ def read_dataframe(filename):
     
     return df
 
-def add_features(train_path="./data/green_tripdata_2021-01.parquet",
-                 val_path="./data/green_tripdata_2021-02.parquet"):
-    df_train = read_dataframe(train_path)
-    df_val = read_dataframe(val_path)
-
+@task
+def add_features(df_train, df_val):
     df_train['PU_DO'] = df_train['PULocationID'] + '_' + df_train['DOLocationID']
     df_val['PU_DO'] = df_val['PULocationID'] + '_' + df_val['DOLocationID']
 
@@ -58,8 +53,8 @@ def add_features(train_path="./data/green_tripdata_2021-01.parquet",
 
     return X_train, X_val, y_train, y_val, dv
 
+@task
 def train_model_search(train, valid, y_val):
-
     def objective(params):
         with mlflow.start_run():
             mlflow.set_tag("model", "xgboost")
@@ -94,10 +89,12 @@ def train_model_search(train, valid, y_val):
         max_evals=1,
         trials=Trials()
     )
-    return 
+    return
 
+@task
 def train_best_model(train, valid, y_val, dv):
     with mlflow.start_run():
+
         best_params = {
             'learning_rate': 0.09585355369315604,
             'max_depth': 30,
@@ -122,16 +119,29 @@ def train_best_model(train, valid, y_val, dv):
         rmse = mean_squared_error(y_val, y_pred, squared=False)
         mlflow.log_metric("rmse", rmse)
 
-        os.makedirs("./models", exist_ok = False)
         with open("models/preprocessor.b", "wb") as f_out:
             pickle.dump(dv, f_out)
-
         mlflow.log_artifact("models/preprocessor.b", artifact_path="preprocessor")
-        mlflow.xgboost.log_model(booster, artifact_path="models_mlflow")
 
-if __name__ == "__main__":
-    X_train, X_val, y_train, y_val, dv = add_features()
+        mlflow.xgboost.log_model(booster, artifact_path="models_mlflow")
+ 
+ # @flow(task_runner = ConcurrentTaskRunner()) -> Default value but do not work when use with mlflow !
+@flow(task_runner=SequentialTaskRunner())
+def main(train_path: str="./data/green_tripdata_2021-01.parquet",
+        val_path: str="./data/green_tripdata_2021-02.parquet"):
+    
+    mlflow.set_tracking_uri("sqlite:///mlflow.db")
+    mlflow.set_experiment("nyc-taxi-experiment")
+
+    X_train = read_dataframe(train_path)
+    X_val = read_dataframe(val_path)
+    
+    X_train, X_val, y_train, y_val, dv = add_features(X_train, X_val).result()
+    
     train = xgb.DMatrix(X_train, label=y_train)
     valid = xgb.DMatrix(X_val, label=y_val)
+    
     train_model_search(train, valid, y_val)
     train_best_model(train, valid, y_val, dv)
+
+main()
